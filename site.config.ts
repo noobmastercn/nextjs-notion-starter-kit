ac443cef4ce9d0683d557db667f91feb8135c4a4
lilpum <lilpum@qq.com>
2024-05-14 20:25:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: '',
-  github: 'noobmastercn',
+  // github: 'noobmastercn',
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
